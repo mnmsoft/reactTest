c89f6d56af21e3419a402fa8d4bf49b9ee5ede39
mnmsoft <76546321+mnmsoft@users.noreply.github.com>
2021-01-18 14:16:33 +0900

    test

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
-import logo from './logo.svg';
+import Subject from "./components/Subject";
+import Nav from  "./components/Nav";
+import Content from "./components/Content";
+import React , { Component } from "react";
 import './App.css';
 
-function App() {
-  return (
-    "hello"
-  );
+
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      subject:{title:'WEB',sub:'World wide web'}
+    };
+  }
+
+  render(){
+    return (
+      <div className="App">
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
+        <Nav></Nav>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
 
 export default App;
