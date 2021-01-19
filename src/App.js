eacba6eb8198071e022f02992e0285ef1ce31e03
mnmsoft <76546321+mnmsoft@users.noreply.github.com>
2021-01-19 15:07:40 +0900

    reactTest

--- src/App.js
+++ src/App.js
@@ -10,19 +10,58 @@ class App extends Component{
   constructor(props){
     super(props);
     this.state = {
-      subject:{title:'WEB',sub:'World wide web'}
+      mode:'read',
+      selectId:1,
+      subject:{title:'WEB',sub:'World wide web2'},
+      welcome:{title:'Welcome',desc:'welcome react!!'},
+      contents:[
+        {id:1,title:'HTML',desc:'HTML is HyperText ...'},
+        {id:2,title:'CSS',desc:'CSS is design ...'},
+        {id:3,title:'javascript',desc:'script ...'},
+      ]
+
     };
   }
 
   render(){
+    console.log("app render");
+    console.log(this.state.mode);
+    var _title , _desc;
+    if (this.state.mode === 'welcome'){
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }else{
+      var i = 1;
+      while(i <= this.state.contents.length){
+        if (i === this.state.selectId){
+          _title = this.state.contents[i-1].title;
+          _desc = this.state.contents[i-1].desc;                
+          break;
+        }
+        i = i+ 1;
+      }
+
+    }
+
+
     return (
       <div className="App">
-        <Subject title={this.state.subject.title} sub={this.state.subject.sub}></Subject>
-        <Nav></Nav>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <Subject title={this.state.subject.title} sub={this.state.subject.sub} onChangePage={function(){
+          this.setState({mode:'welcome'});
+        }.bind(this)}></Subject>
+        <Nav data={this.state.contents} onChangePage={function(id){
+          this.setState({
+            mode:'read',
+            selectId : Number(id)
+          });
+        }.bind(this)}></Nav>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
 }
+
+
+
 
 export default App;
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,23 @@
 import React , {Component} from 'react';
 
 class Nav extends Component{
+
     render(){
+        var lists = [];
+        var data = this.props.data;
+        var i = 1;
+        while(i<=data.length){
+            lists.push(<li key={data[i-1].id}><a href={"./"} data-id={data[i-1].id} onClick={function(e){
+                e.preventDefault();
+                this.props.onChangePage(e.target.dataset.id);
+            }.bind(this)}>{data[i-1].title}</a></li>);
+            i = i + 1;
+        }
+
         return(
             <nav>
                 <ul>
-                  <li><a href="1.html">HTML</a></li>
-                  <li><a href="2.html">CSS</a></li>
-                  <li><a href="3.html">JAVASCRIPT</a></li>
+                    {lists}
                 </ul>
             </nav>
         );
